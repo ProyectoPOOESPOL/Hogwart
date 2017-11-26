5e3db3316bc6b34b255c74189b8999c15dec6c74
yoston <yostoncornet@hotmail.es>
2017-11-26 16:18:02 -0500

    Clase planificador creada

---
+++ src/proyectopoo/Planificador.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopoo;
+
+/**
+ *
+ * @author Jhonston
+ */
+public class Planificador {
+    public void crearCurso(){
+        
+    }
+    public void crearProfesor(){
+        
+    }
+    public void crearEstudiante(){
+        
+    }
+    public void verHorarios(){
+        
+    }
+    public void listadoEstudiantes(){
+        
+    }
+}
